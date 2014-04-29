7c7a36cf94dc1def8df5259b171aca03804a10f4
Shivam Pandya <contact@tutorialdrive.org>
2014-04-29 23:54:02 +0530

    Add Required Files

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/* ===========================================================
+ * script.js v1.0
+ * ===========================================================
+ * Copyright 2014 Shivam Pandya - Tutorial Drive.
+ * http://www.tutorialdrive.org
+ *
+ * Create an Vertical Thumbnail Carousel For Twitter Bootstrap v3.0.3
+ *
+ * 
+ * License: MIT License
+ * http://opensource.org/licenses/MIT
+ *
+ * ========================================================== */
+$(document).ready(function() {
+	$('#myCarousel').carousel({
+		interval: false
+	})
+});
